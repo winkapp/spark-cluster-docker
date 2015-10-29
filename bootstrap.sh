00337f82662abc8746523eff074d9b9693c90e3c
Nathan Smith <nathansmith22@gmail.com>
2015-10-29 15:11:52 -0400

    [bootstrap] Added spark master host option

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-
 CMD=${1:-"exit 0"}
 if [[ "$CMD" == "master" ]]; then
-  /bin/bash -c "$SPARK_HOME/sbin/start-master.sh"
+  /bin/bash -c "$SPARK_HOME/sbin/start-master.sh -h $SPARK_MASTER_HOST"
+  /bin/bash -c "tail -f $SPARK_HOME/logs/spark--org.apache.spark.deploy.master.Master*"
 elif [[ "$CMD" == "slave" ]]; then
   /bin/bash -c "$SPARK_HOME/sbin/start-slave.sh $SPARK_MASTER_URL"
+  /bin/bash -c "tail -f $SPARK_HOME/logs/spark--org.apache.spark.deploy.worker.Worker*"
 else
   /bin/bash -c "$*"
 fi
