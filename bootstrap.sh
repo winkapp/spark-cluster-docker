ffe9029e13679dd01ddabc276e4ab998a63524c9
Gabe Ochoa <gabeochoa@gmail.com>
2017-01-11 12:01:40 -0500

    Adding configurable master and worker memory

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 CMD=${1:-"exit 0"}
+MASTER_MEM = ${SPARK_MASTER_MEM:-"1G"}
+WORKER_MEM = ${SPARK_WORKER_MEM:-"1G"}
+
 if [[ "$CMD" == "master" ]]; then
-  /bin/bash -c "$SPARK_HOME/sbin/start-master.sh -h $SPARK_MASTER_HOST"
+  /bin/bash -c "$SPARK_HOME/sbin/start-master.sh -m $MASTER_MEM -h $SPARK_MASTER_HOST"
   /bin/bash -c "tail -f $SPARK_HOME/logs/spark--org.apache.spark.deploy.master.Master*"
 elif [[ "$CMD" == "worker" ]]; then
-  /bin/bash -c "$SPARK_HOME/sbin/start-slave.sh $SPARK_MASTER_URL"
+  /bin/bash -c "$SPARK_HOME/sbin/start-slave.sh -m $WORKER_MEM $SPARK_MASTER_URL"
   /bin/bash -c "tail -f $SPARK_HOME/logs/spark--org.apache.spark.deploy.worker.Worker*"
 else
   /bin/bash -c "$*"
