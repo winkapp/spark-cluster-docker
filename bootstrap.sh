d96e08730a892c8c27aebe190a8d46232278be68
kalman <kal@wink.com>
2017-10-04 15:48:00 -0400

    removes shuffle service (unused) and memory restrictions

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 CMD=${1:-"exit 0"}
-WORKER_MEM=${SPARK_WORKER_MEM:-"1G"}
 
 if [[ "$CMD" == "master" ]]; then
   /bin/bash -c "$SPARK_HOME/sbin/start-master.sh -h $SPARK_MASTER_HOST"
   /bin/bash -c "tail -f $SPARK_HOME/logs/spark--org.apache.spark.deploy.master.Master*"
 elif [[ "$CMD" == "worker" ]]; then
-  /bin/bash -c "$SPARK_HOME/sbin/start-slave.sh -m $WORKER_MEM $SPARK_MASTER_URL"
-  /bin/bash -c "$SPARK_HOME/sbin/start-shuffle-service.sh"
+  /bin/bash -c "$SPARK_HOME/sbin/start-slave.sh $SPARK_MASTER_URL"
   /bin/bash -c "tail -f $SPARK_HOME/logs/spark--org.apache.spark.deploy.worker.Worker* -f $SPARK_HOME/logs/spark--org.apache.spark.deploy.ExternalShuffleService*"
 else
   /bin/bash -c "$*"
